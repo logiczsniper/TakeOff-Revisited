7a9168d511e47c35fd6d533f8aae4f403ffc8107
Logan <lczernel@gmail.com>
2019-02-18 20:21:26 +0000

    Started documentation

--- src/game.py
+++ src/game.py
@@ -9,16 +9,29 @@ from scenes import *
 
 
 class Game:
+    """
+    The entire game is contained in this class or implemented in this class, i.e, all of the scene
+    classes are implemented here.
+    """
+
     def __init__(self):
-        self.num_of_enemies = 2
+
+        # Set up display, gain access to all the resources, make these resources accessible throughout the class
         self.screen = display.set_mode((Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT))
         display.set_caption('TakeOff')
         self.resources = Resources()
         display.set_icon(self.resources.ICON)
+
+        # Set up groups
         self.enemies = Group()
         self.rocket_group = GroupSingle()
+
+        # Create the player (rocket)
         self.rocket = Rocket((20, 20), self.rocket_group)
+
         self.all_sprites = Group(self.rocket, self.enemies)
+
+        # Create a generator so I can use the next() to get the next enemy that will be displayed
         self.current_enemy_generator = self.generate_sprite_sheet()
         self.current_enemy = next(self.current_enemy_generator)
 
@@ -29,7 +42,10 @@ class Game:
         # Must set the on click lambda after creation as it needs access to switch_to_scene
         self.active_scene.set_on_click(
             lambda: self.active_scene.switch_to_scene(LevelScene(self.resources.FIRST_BG, 2)))
+
+        # Set initial values for the altitude text and the number of enemies
         self.altitude_text = ""
+        self.num_of_enemies = 2
 
         # Init mixer, start playing background music
         mixer.init()
@@ -37,10 +53,17 @@ class Game:
         mixer.music.set_volume(0.5)
         mixer.music.play(-1)
 
+        # Set up loop, clock
         self.done = False
         self.clock = time.Clock()
 
     def run(self):
+        """
+        Runs the game in a while loop.
+        First, the newest pygame events are handled and passed to each sprite. Next, each sprite is updated and core
+        game logic is applied. Next, all of the sprites and background are drawn before the display is updated, and the
+        clock tick continues.
+        """
 
         while not self.done:
             self.event_loop()
@@ -50,6 +73,11 @@ class Game:
             self.clock.tick(Constants.FPS)
 
     def event_loop(self):
+        """
+        Handle all the events from pygame, the main event being mouse motion (as the rocket must follow this) and mouse
+        click, which this function depends on the scene.
+        """
+
         for event in py_event.get():
             if event.type == QUIT:
                 self.done = True
@@ -64,6 +92,9 @@ class Game:
                                                     self.active_scene.current_level), self.active_scene)
 
     def update(self):
+        """
+        Apply core game logic including spawning enemies, detecting collisions and incrementing level.
+        """
 
         # Spawn enemies
         if len(self.enemies) < self.num_of_enemies and isinstance(self.active_scene,
@@ -110,6 +141,8 @@ class Game:
         elif self.rocket.elevation == 2000:
             self.increment_level(self.resources.THIRD_BG, 4)
         elif self.rocket.elevation == 2500:
+
+            # Increment number of enemies for the final 500 km
             self.num_of_enemies += 1
         elif self.rocket.elevation == 3000:
 
@@ -137,9 +170,21 @@ class Game:
             self.active_scene = StaticScene(self.resources.CRASH_BG, Constants.MESSAGE.format("quit"),
                                             Constants.WINDOW_WIDTH / 2 - 110, lambda: quit())
 
+        # Update the active scene if changed
         self.active_scene = self.active_scene.next
 
     def increment_level(self, next_background, next_scroll_speed):
+        """
+        Increase the level by one, which includes switching to the MidLevelScene. Resets game by clearing enemies,
+        increments difficulty by increasing num_of_enemies, and gets the next() enemy.
+
+        :param next_background: the resource for the background image of the next scene.
+        :type: Surface
+
+        :param next_scroll_speed: the speed of the scrolling background for the next scene.
+        :type: int
+        """
+
         self.active_scene = MidLevelScene()
         self.active_scene.render(self.screen)
         self.active_scene = LevelScene(next_background, next_scroll_speed)
@@ -148,15 +193,26 @@ class Game:
         self.current_enemy = next(self.current_enemy_generator)
 
     def kill_enemies(self):
+        """
+        Kills all enemy sprites
+        """
+
         for enemy in self.enemies:
             enemy.kill()
 
     def generate_sprite_sheet(self):
+        """
+        Generate the next enemy sprite sheet on request (will be requested each time the level is incremented).
+        """
 
         for sheet in [self.resources.BIRD_SHEET, self.resources.HELICOPTER_SHEET, self.resources.SATELLITE_SHEET]:
             yield sheet
 
     def draw(self):
+        """
+        Render the active scene, display the altitude, draw all sprites.
+        """
+
         self.active_scene.render(screen=self.screen)
         self.screen.blit(self.altitude_text, (3, 3))
         self.all_sprites.draw(self.screen)
--- src/resources.py
+++ src/resources.py
@@ -3,22 +3,53 @@ from pygame import image, mixer
 
 
 class Resources:
+    """
+    A single class that holds all of the game assets
+    """
 
-    # Image Loader
     @staticmethod
     def load_image(folder, name):
+        """
+        Uses the image.load method to load an image from a path that is created by formatting the base path in Constants
+        with the param folder and name.
+
+        :param folder: the name of the folder that contains the image
+        :param name: the name of the file of the image
+        :type: str
+
+        :return: image that has been loaded
+        :rtype: Surface
+        """
 
         loaded_image = image.load(Constants.BASE_IMAGE_PATH.format(folder, name)).convert_alpha()
         return loaded_image
 
-    # Sound loader
     @staticmethod
     def load_sound(folder, name, volume):
+        """
+        Uses the mixer.Sound class to create a sound object using the path that is created by formatting the base path
+        in Constants with the folder and name parameters. Then sets the volume of the sound to the given volume param.
+
+        :param folder: the name of the folder that contains the sound
+        :param name: the name of the file of the sound
+        :type: str
+
+        :param volume: the volume level that the sound will be set to
+        :type: int
+
+        :return: the sound that has been loaded and volume adjusted
+        :rtype: Sound
+        """
+
         loaded_sound = mixer.Sound(Constants.BASE_SOUND_PATH.format(folder, name))
         loaded_sound.set_volume(volume)
         return loaded_sound
 
     def __init__(self):
+        """
+        Loads all of the images and sounds.
+        """
+
         # Images
         self.ICON = image.load(Constants.WINDOW_ICON).convert_alpha()
         self.ROCKET_SHEET = self.load_image("sprites", "rocket")
